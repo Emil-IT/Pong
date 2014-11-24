e24139416df87322a569601481a24c3a67ba46e2
Emil-IT <emil.osterberg90@gmail.com>
2014-11-24 13:58:17 +0100

    Fastnar inte i taket längre

--- src/model/MyPongModel.java
+++ src/model/MyPongModel.java
@@ -8,35 +8,37 @@ import java.util.Map;
 import java.util.HashMap;
 
 
+
 public class MyPongModel implements PongModel {
+	private class MyPoint extends Point{
+		double x;
+		double y;
+	}
 	
 	private Map<BarKey, Integer> barPos = new HashMap<BarKey, Integer>();
 	private Map<BarKey, Integer> barHeight = new HashMap<BarKey, Integer>();
 	private String leftPlayer;
 	private String rightPlayer;
-	private Point BallPos;
 	private String message = "hej!!!";
 	private Map<BarKey, Integer> score = new HashMap<BarKey, Integer>();
 	private Dimension fieldSize;
 	private Point ballPos = new Point();
-	private int ballSpeed = 3;
+	private int ballSpeed;
 	private double ballDir;
 	
 	public MyPongModel(String leftPlayer, String rightPlayer){
 		fieldSize = new Dimension();
-		fieldSize.setSize(150, 100);
+		fieldSize.setSize(1500, 1000);
 		this.leftPlayer = leftPlayer;
 		this.rightPlayer = rightPlayer;
-		barPos.put(BarKey.LEFT, 10);
-		barPos.put(BarKey.RIGHT, 10);
-		barHeight.put(BarKey.LEFT, 20);
-		barHeight.put(BarKey.RIGHT, 20);
+		barPos.put(BarKey.LEFT, 100);
+		barPos.put(BarKey.RIGHT, 100);
+		barHeight.put(BarKey.LEFT, 200);
+		barHeight.put(BarKey.RIGHT, 200);
 		score.put(BarKey.LEFT, 0);
 		score.put(BarKey.RIGHT, 0);
-		ballPos.setLocation(75, 50);
+		ballPos.setLocation(750, 500);
 		resetBall();
-		
-		
 		
 		
 	}
@@ -46,47 +48,72 @@ public class MyPongModel implements PongModel {
 		while(iterator.hasNext()){
 			moveBar(iterator.next(), delta_t);
 		}
+		message = ("X=: " + ballPos.x + ", Y=: " + ballPos.y);
 		moveBall(delta_t);
 		
 	}
 	
 	private void moveBar(Input input, long delta_t){
 		int pos = getBarPos(input.key);
-		if(input.dir == Input.Dir.UP && pos > 10){
-			pos -= 0.05 * delta_t;
+		if(input.dir == Input.Dir.UP && pos > 100){
+			pos -= 0.55 * delta_t;
 		}
-		if(input.dir == Input.Dir.DOWN && pos < 90){
-			pos += 0.07 * delta_t;
+		if(input.dir == Input.Dir.DOWN && pos < 900){
+			pos += 0.75 * delta_t;
 		}
 		setBarPos(input.key, pos);
 	}
 	
 	private void moveBall(long delta_t){
-		ballPos.x += ballSpeed * Math.cos(ballDir);
-		ballPos.y += ballSpeed * Math.sin(ballDir);
-		if(ballPos.x >= 150){
-			if(Math.abs(ballPos.y - getBarPos(BarKey.RIGHT)) > 10){
+		ballPos.x += ballSpeed * Math.cos(ballDir) * 0.03 * delta_t;
+		ballPos.y -= ballSpeed * Math.sin(ballDir) * 0.03 * delta_t;
+		
+		//Bollen träffar högerkanten
+		if(ballPos.x >= 1500){
+			if(Math.abs(ballPos.y - getBarPos(BarKey.RIGHT)) > 100){
 				score.put(BarKey.LEFT, score.get(BarKey.LEFT) + 1);
 				resetBall();
-			}else ballDir = Math.PI - ballDir;
+			}
+			else{
+				ballSpeed++;
+				if(Math.abs(ballPos.y - getBarPos(BarKey.RIGHT)) <= 70){
+					ballDir = Math.PI - ballDir;
+				}else{
+						ballDir += Math.PI;
+				}
+				
+			}
 		}
+		
+		//Bollen träffar vänsterkanten
 		if(ballPos.x <= 0){ 
-			if(Math.abs(ballPos.y - getBarPos(BarKey.LEFT)) > 10){
+			if(Math.abs(ballPos.y - getBarPos(BarKey.LEFT)) > 100){
 				score.put(BarKey.RIGHT, score.get(BarKey.RIGHT) + 1);
 				resetBall();
-			}else ballDir = Math.PI - ballDir;
+			}
+			else{
+				ballSpeed++;
+				if(Math.abs(ballPos.y - getBarPos(BarKey.LEFT)) <= 70){
+					ballDir = Math.PI - ballDir;
+				}else{
+						ballDir += Math.PI;
+				}
+			}
 		}
-		if(ballPos.y > 100 || ballPos.y < 0){
-			ballDir = 2*Math.PI - ballDir;
-			System.out.println("" + ballDir);
+		
+		//Bollen träffar golvet/taket
+		if(ballPos.y >= 1000 || ballPos.y <= 0){
+			ballDir *= -1;
+			
 		}
 	}
 		
 	 private void resetBall(){
-		 ballPos.setLocation(75, 50);
+		 ballPos.setLocation(750, 500);
 		 ballDir = Math.random()*2.0*Math.PI;
 		 while(ballDir > Math.PI / 4 && ballDir < 3 * Math.PI / 4 || ballDir > 5 * Math.PI / 4 && ballDir < 7 * Math.PI / 4) ballDir = Math.random()*2.0*Math.PI;
-		 ballSpeed = 3;
+		 ballSpeed = 20;
+		 //ballDir = Math.PI / 3;
 	 }
 	 
 	 public int getBarPos(BarKey k){
